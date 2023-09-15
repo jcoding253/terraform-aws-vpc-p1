7a3ce8459e41e3450d7a04bd9cb4a37d6be030b8
jchurch133 <jchurch133@gmail.com>
2023-09-15 09:24:18 -0500

    main: changed sg style

--- main.tf
+++ main.tf
@@ -84,7 +84,36 @@ resource "aws_security_group" "allow-web-1" {
   name        = "allow_web_traffic"
   description = "allow_tcp_traffic"
   vpc_id      = aws_vpc.prod-vpc-1.id
+  ingress {
+    description = "ssh"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol = "tcp"
+  }
 
+  ingress {
+    description = "http"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol = "tcp"
+  }
+
+  ingress {
+    description = "https"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 443
+    to_port     = 443
+    protocol = "tcp"
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
   tags = {
     Name = "allow-web-1"
   }
@@ -93,36 +122,6 @@ resource "aws_security_group" "allow-web-1" {
   lifecycle {
     create_before_destroy = true
   }
-}
-
-resource "aws_vpc_security_group_ingress_rule" "ingress_ssh" {
-  security_group_id = aws_security_group.allow-web-1.id
-  cidr_ipv4 = "0.0.0.0/0"
-  from_port   = 22
-  to_port     = 22
-  ip_protocol = "tcp"
-}
-
-resource "aws_vpc_security_group_ingress_rule" "ingress_http" {
-  security_group_id = aws_security_group.allow-web-1.id
-  cidr_ipv4 = "0.0.0.0/0"
-  from_port   = 80
-  to_port     = 80
-  ip_protocol = "tcp"
-}
-
-resource "aws_vpc_security_group_ingress_rule" "ingress_https" {
-  security_group_id = aws_security_group.allow-web-1.id
-  cidr_ipv4 = "0.0.0.0/0"
-  from_port   = 443
-  to_port     = 443
-  ip_protocol = "tcp"
-}
-
-resource "aws_vpc_security_group_egress_rule" "egress_all" {
-  security_group_id = aws_security_group.allow-web-1.id
-  cidr_ipv4 = "0.0.0.0/0"
-  ip_protocol = "-1"
 }
 
 
